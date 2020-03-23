651995fdbdef8cc8c45cd4707114d286eca4e0fa
Njoku Godswill <senzaspirit@outlook.com>
2020-03-23 21:52:34 +0800

    Register  DB tables
    The table for the basic register
    system.

---
+++ InvSysData/dbo/Tables/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AuthUserid] NVARCHAR(128) NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(256) NOT NULL, 
+    [CreatedDated] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
