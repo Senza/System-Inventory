de1219b32c8b42031fbf084400ef2e8825263299
Njoku Godswill <senzaspirit@outlook.com>
2020-05-05 20:27:42 +0800

    Update EF Database Name
    Updates the EF database name to be
    EFData

--- InvSysUI/ViewModels/ShellViewModel.cs
+++ InvSysUI/ViewModels/ShellViewModel.cs
@@ -10,20 +10,19 @@ namespace InvSysUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        private LoginViewModel _loginVM;
+      
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        private SimpleContainer _container;
+   
 
         public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, SimpleContainer container)
         {
             _events = events;
             _salesVM = salesVM;
-            _container = container;
 
             _events.Subscribe(this);
 
-            ActivateItem(_container.GetInstance<LoginViewModel>());
+            ActivateItem(IoC.Get<LoginViewModel>());
 
         }
 
